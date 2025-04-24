113c6ec287805ab52dbb21bd9540f053b577e960
Jetissha-Gautam <24bit179@sot.pdpu.ac.in>
2025-04-24 22:51:26 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,10 @@
+#1.	Add two numbers.
+print("Enter two numbers to add:")
+
+num1 = float(input("First number: "))
+num2 = float(input("Second number: "))
+
+sum_result = num1 + num2
+
+print("The sum of", num1, "and", num2, "is:", sum_result)
+
